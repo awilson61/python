2e0c4bac547477b3977000abe15d3c4870753134
awilson61 <144381426+awilson61@users.noreply.github.com>
2023-11-08 19:18:15 -0600

    More progress

--- test_television.py
+++ test_television.py
@@ -1,11 +1,20 @@
-import unittest
+import pytest
+from television import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        pass
-        #self.assertEqual(True, False)  # add assertion here
+class Test:
+    def setup_method(self):
+        self.tv1 = Television()
 
+    def teardown_method(self):
+        del self.tv1
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_init(self):
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_power(self):
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        self.tv1.power()
+        assert self.tv1.__str__() == 'Power = False, Channel = 0, Volume = 0'
